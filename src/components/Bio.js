2ff929d24635750ebb0c34f96c4d27ab2eaa10b5
Ivan Malagon <hacheka@gmail.com>
2018-03-18 22:54:01 +0100

    House cleaning

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -18,7 +18,7 @@ class Bio extends React.Component {
       >
         <img
           src={profilePic}
-          alt={`Kyle Mathews`}
+          alt={`Ivan Malagon`}
           style={{
             marginRight: rhythm(1 / 2),
             marginBottom: 0,
